19e0ae77c1bd4efebb20868e0f577ea3f1ea0d16
Alejandro Sandrin <alejandro.sandrin@wirsolut.com>
2025-11-07 20:36:30 -0300

    - Se remueve librería `material ui` y se reemplaza por `angular/material`. - Aplicados estilos en `navbar`, distribución de componentes en componente `app`.

--- web/src/core/footer/footer.component.ts
+++ web/src/core/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
 
+export class FooterComponent implements OnInit {
+  year: number = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    const currentDate = new Date();
+    this.year = currentDate.getFullYear();
+  }
+  
 }
--- web/src/core/navbar/navbar.component.ts
+++ web/src/core/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
 
 }
