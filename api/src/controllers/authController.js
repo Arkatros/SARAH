3fb9adade02451893e47e1b06d0193dca511fec7
Joni <jonatan_massacesi@hotmail.com>
2025-11-08 10:25:34 -0300

    Add prisma schemes,add login and register endpoints

---
+++ api/src/controllers/authController.js
@@ -0,0 +1,16 @@
+import * as userService from "../services/authService.js";
+
+export const loginMidwife = async (req, res) => {
+  const tokengen = await userService.loginMidwife(req.body);
+
+  if (tokengen) {
+    res.json({
+      message: "Succesfull authentication",
+      data: tokengen,
+    });
+  } else {
+    res.status(401).json({ mensaje: "Credenciales inválidas", data: null });
+  }
+};
+
+//lOGIN ADMIN, QUIZA?
