fbf211d2c733abc0bc987ea60f74120bff61e4b4
Alejandro Sandrin <alejandro.sandrin@wirsolut.com>
2025-11-08 21:01:40 -0300

    - Fix render de `router-outlet`. - Decalración y uso de variables globales en `styles.css` y otros componentes. - Ajustes estéticos varios en `navbar`. - Agregada info de proyecto a la `home`.

--- web/src/core/navbar/navbar.component.ts
+++ web/src/core/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
