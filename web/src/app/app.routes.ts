281d348a475d4c929cddbc88667606315db28172
Alejandro Sandrin <alejandro.sandrin@wirsolut.com>
2025-11-08 21:13:01 -0300

    - Creado nuevo componente  `not-found`, ruta registrada.

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from '../pages/home/home';
+import { NotFound } from '../pages/not-found/not-found';
 
 export const routes: Routes = [
-    {path: "", component: Home},
+    { path: "", component: Home },
+    { path: '**', component: NotFound } // <---- Si van a registrar rutas, esta DEBE SER LA ULTIMA EN LA LISTA SIEMPRE
 ];
