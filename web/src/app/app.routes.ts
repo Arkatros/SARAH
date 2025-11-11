effbbaecbe558233f7bf93f055d0a114d4207125
Alejandro Sandrin <alejandro.sandrin@wirsolut.com>
2025-11-11 18:53:17 -0300

    - Agregada nueva vista `about`. - Fix estilos en `navbar`.

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NotFound } from '../pages/not-found/not-found';
 import { ANRQ } from '../pages/tests/anrq/anrq';
 import { EPDS } from '../pages/tests/epds/epds';
 import { PASS } from '../pages/tests/pass/pass';
+import { About } from '../pages/about/about';
 import { PatientRegisterComponent } from '../pages/patient/patient-register.component';
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: "tests/anrq", component: ANRQ },
     { path: "tests/epds", component: EPDS },
     { path: "tests/pass", component: PASS },
+    { path: "about", component: About },
     { path: "patient/register", component: PatientRegisterComponent },
     { path: '**', component: NotFound } // <---- Si van a registrar rutas, esta DEBE SER LA ULTIMA EN LA LISTA SIEMPRE
 ];
